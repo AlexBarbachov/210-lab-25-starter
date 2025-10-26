14f9763fc2a7e6b369807ba042ebc447c365b29c
AlexBarbachov <66849795+AlexBarbachov@users.noreply.github.com>
2025-10-26 16:19:23 -0700

    readlines function, thinking bout sol

--- main.cpp
+++ main.cpp
@@ -6,9 +6,25 @@
 #include <string>
 #include <iomanip>
 #include <algorithm>
+#include <fstream>
 using namespace std;
 
+// read file
+vector<string> read_file(const string& filename)
+{
+    ifstream fin(filename);
+    vector<string> data;
+    string line;
+
+    while (getline(fin, line))
+        data.push_back(line);
+    return data;
+}
+
+
 int main() {
+    const int numStuff = 20000;
+    vector<string> v = read_file("codes.txt");
     
 
     return 0;
