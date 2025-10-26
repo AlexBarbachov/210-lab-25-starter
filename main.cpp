799904baf0782e0fc9e2a4493cc18d01c5abc830
AlexBarbachov <66849795+AlexBarbachov@users.noreply.github.com>
2025-10-26 16:54:53 -0700

    fourth race, + output

--- main.cpp
+++ main.cpp
@@ -103,7 +103,7 @@ int main() {
     {
         list<string> l(lines.begin(), lines.end());
 
-        
+
         clock_t start = clock();
         list<string>::iterator it = l.begin();
         for (size_t i = 0; i < l.size()/2; ++i) ++it;
@@ -121,12 +121,51 @@ int main() {
         times[2][2] = (long long)((end - start) * 1000000 / CLOCKS_PER_SEC);
     }
 
+    // ********************* RACE 4 *************************
+    {
+        vector<string> v = lines;
+        clock_t start = clock();
+        v.erase(v.begin() + v.size()/2);
+        clock_t end = clock();
+        times[3][0] = (long long)((end - start) * 1000000 / CLOCKS_PER_SEC);
+    }
+
+    {
+        list<string> l(lines.begin(), lines.end());
+        list<string>::iterator it = l.begin();
+        clock_t start = clock();
+        for (size_t i = 0; i < l.size()/2; ++i) ++it;
+
+        l.erase(it);
+        clock_t end = clock();
+        times[3][1] = (long long)((end - start) * 1000000 / CLOCKS_PER_SEC);
+    }
+
+    {
+        set<string> s(lines.begin(), lines.end());
+        set<string>::iterator it = s.begin();
+        clock_t start = clock();
+        for (size_t i = 0; i < s.size()/2; ++i) ++it;
+
+        s.erase(it);
+        clock_t end = clock();
+        times[3][2] = (long long)((end - start) * 1000000 / CLOCKS_PER_SEC);
+    }
+
 
     // output
-    cout << "V " << times[2][0] << endl;
-    cout << "L " << times[2][1] << endl;
-    cout << "S " << times[2][2] << endl;
+    cout << left << setw(12) << "Operation" << setw(12) << "Vector" << setw(12) << "List" << setw(12) << "Set" << endl;
 
+    const char* names[4] = {"Read", "Sort", "Insert", "Delete"};
+    for (int i = 0; i < 4; i++)
+    {
+        cout << left << setw(12) << names[i];
+        for (int j = 0; j < 3; j++)
+        {
+            cout << setw(12) << times[i][j];
+        }
+        cout << endl;
+    }
 
 
 
